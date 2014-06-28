de606764a4665973ec3912f144e0aaae42523013
Nima Esmaili Mokaram <esmailimokaram.1@osu.edu>
2014-06-28 11:28:36 -0400

    Added Systems folder for CI
    Added the systems folder and change the path in

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ if (defined('ENVIRONMENT'))
  * as this file.
  *
  */
-	$system_path = 'C:\Program Files (x86)\Zend\Apache2\web_lib\CI_system';
+	$system_path = 'CI_system';
 
 /*
  *---------------------------------------------------------------
